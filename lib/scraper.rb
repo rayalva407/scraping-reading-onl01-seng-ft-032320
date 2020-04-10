a43e0749d179c3fcefa223d13d9b65ba71d3a491
Ray Alva <rayalva407@gmail.com>
2020-04-10 17:52:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,4 +5,5 @@ html = open("https://flatironschool.com/")
 
 Nokogiri::HTML(html)
 
-doc = Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-260IBN")
